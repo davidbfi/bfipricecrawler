f531385fe230b830b39c9f44db4c867d0c988f52
davidbfi <david.hutape@bfi.co.id>
2021-06-03 15:50:08 +0700

    Update : add fields (deskripsi, waktu crawling, lokasi) and change source to sumber

--- bfipricecrawler/items.py
+++ bfipricecrawler/items.py
@@ -18,14 +18,17 @@ class CarItem(scrapy.Item):
     model = scrapy.Field()
     varian = scrapy.Field()
     transmisi = scrapy.Field()
+    lokasi = scrapy.Field()
     provinsi = scrapy.Field()
     kabupaten_kecamatan = scrapy.Field()
     harga = scrapy.Field()
     url = scrapy.Field()
     tipe_penjual = scrapy.Field()
-    source = scrapy.Field()
+    sumber = scrapy.Field()
     spesifikasi_ringkas = scrapy.Field()
+    deskripsi = scrapy.Field()
     tanggal_diperbaharui_sumber = scrapy.Field()
+    waktu_crawl = scrapy.Field()
 
 
 class SpesifikasiItem(scrapy.Item):
