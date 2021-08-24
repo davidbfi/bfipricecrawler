0c9721a92f2aad24a8846b3ed23e7d7382558338
davidbfi <david.hutape@bfi.co.id>
2021-08-24 17:27:22 +0700

    Update : change html target after revamp on targeted website

--- bfipricecrawler/spiders/mobil123.py
+++ bfipricecrawler/spiders/mobil123.py
@@ -2,26 +2,21 @@ import json
 import requests
 import itertools
 import pandas as pd
-from bs4 import BeautifulSoup
 
 import scrapy
 from scrapy import Request
-from scrapy.spiders import CrawlSpider, Rule
-from scrapy.linkextractors import LinkExtractor
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from scrapy_selenium import SeleniumRequest
-
 from bfipricecrawler.utils.utils import ExpectedConditionModifier
 from bfipricecrawler.items import CarItem
-from bfipricecrawler.utils.utils import list_to_dict, clean_price
+from bfipricecrawler.utils.utils import list_to_dict, price_parser, category_parser, date_parser, seller_parser, location_parser, tab_specifications_parser
 
 
 def get_url():
     data = pd.read_csv('/home/david/Desktop/NOTEBOOK/NLP/list_url_mobil123.csv')
     urls = data['urls']
-    return urls
+    urls_ = []
+    for url in urls:
+        urls_.append(url)
+    return urls_
 
 
 data = get_url()
@@ -29,71 +24,41 @@ data = get_url()
 
 class Mobil123Crawler(scrapy.Spider):
     name = 'mobil123'
-    urls = data
-    start_urls = urls
-
-    def start_requests(self):
-        for url in self.start_urls:
-            yield SeleniumRequest(
-                url=url,
-                callback=self.parse,
-                wait_time=1,
-                wait_until=ExpectedConditionModifier(
-                    EC.presence_of_element_located(
-                        (By.CLASS_NAME, "listing__price  delta  weight--bold")),
-                    EC.presence_of_element_located(
-                        (By.CLASS_NAME, "listing__section__head"))
-                )
-            )
+    start_urls = data[200:210]
 
     def parse(self, response):
+        links = response.xpath('/html/body/main/article/section/section[2]/section[1]/div/div/div[1]/div[1]/div/div[1]/div/a[2]/@href').extract()
+        for link in links:
+            yield Request(response.url + link, callback=self.parse_kelengkapan)
+
+    def parse_kelengkapan(self, response):
+        updated_date = date_parser(response.css('span[class="u-color-muted  u-text-7  u-hide@desktop  u-margin-bottom-xs  u-block"]  ::text').extract()[0].strip())
+        category = category_parser(response.css('ul[class="c-breadcrumb  u-text-7"] ::text').extract())
+        name = response.css('h1[class="listing__title  u-color-dark  u-margin-bottom-xs  u-text-3  u-text-4@mobile  u-text-bold"]  ::text').extract()[0].strip()
+        price = price_parser(response.css('div[class="listing__item-price"] ::text').extract()[1])
+        summary_spesifications = list_to_dict(response.css('div[class="u-width-4/6  u-width-1@mobile"]  ::text').extract())
+        location = location_parser(response.css('span[class="c-chip  c-chip--sm  u-rounded  u-margin-right-xxs  c-chip--wrap "]  ::text').extract() + response.css('span[class="c-chip  c-chip--sm  u-rounded  u-margin-right-xxs c-chip--wrap "]  ::text').extract())
+        seller_type = seller_parser(response.css('span[class="c-chip  c-chip--icon  u-rounded  c-chip--sm  u-margin-right-xxs  u-margin-bottom-xxs"] ::text').extract())
+        # spesifications_tab = tab_specifications_parser(response.css('div[id="tab-specifications"] ::text').extract())
+        # equipments_tab = list_to_dict(response.css('div[id="tab-equipments"] ::text').extract())
+        # seller = list_to_dict((response.css('div[id="tab-seller-notes"] ::text').extract()))
         item = CarItem()
         item["url"] = response.url
-        tabel_spesifikasi_ringkas = response.css('div[class="listing__key-listing__list"] ::text').extract()
-        spesifikasi_ringkas = list_to_dict(tabel_spesifikasi_ringkas)
-        informasi_penjual = response.css('div[class="list-item  soft-half--ends"] ::text').extract()
-        update_date = response.css('div[class="listing__updated  visuallyhidden--palm"] ::text').extract_first().split(':')[-1]
-        item["nama"] = response.css('h1[class="headline  listing__title  delta  flush--bottom"] ::text').extract_first()
-        item["merek"] = spesifikasi_ringkas.get('Merek') or ""
-        item["model"] = spesifikasi_ringkas.get('Model') or ""
-        item["varian"] = spesifikasi_ringkas.get('Varian') or ""
-        item["transmisi"] = spesifikasi_ringkas.get('Transmisi') or ""
-        item["tahun"] = spesifikasi_ringkas.get('Tahun') or ""
-        item["warna"] = spesifikasi_ringkas.get('Warna') or ""
-        item["harga"] = int(clean_price(response.css('div[class="listing__price  delta  weight--bold"] ::text').extract_first()))
-        item['provinsi'] = informasi_penjual[-1].split('»')[0].strip()
-        item['kabupaten_kecamatan'] = informasi_penjual[-1].split('»')[-1].strip()
-        item['tipe_penjual'] = informasi_penjual[1].strip()
-        item['tanggal_diperbaharui_sumber'] = update_date.strip()
-        item['spesifikasi_ringkas'] = spesifikasi_ringkas
+        item["nama"] = name
+        item["merek"] = category.get('Merk') or ''
+        item["model"] = category.get('Model') or ''
+        item["varian"] = category.get('Variant') or ''
+        item["transmisi"] = summary_spesifications.get('Transmisi') or ''
+        item["warna"] = summary_spesifications.get('Warna') or ''
+        item["tahun"] = summary_spesifications.get('Tahun') or ''
+        item["harga"] = int(price) or ''
+        item['provinsi'] = location.get('provinsi').strip()
+        item['kabupaten_kecamatan'] = location.get('kabupaten_kota').strip()
+        item['tipe_penjual'] = seller_type
+        item['tanggal_diperbaharui_sumber'] = updated_date.strip()
+        item['spesifikasi_ringkas'] = summary_spesifications
         item['sumber'] = "Mobil123"
 
-        # post_url = "http://localhost:9200/seachcar/cars"
-        # payload = {
-        #     "nama": item["nama"],
-        #     "merek": item["merek"],
-        #     "model": item["model"],
-        #     "varian": item["varian"],
-        #     "provinsi": item["provinsi"],
-        #     "kabupaten_kecamatan": item["kabupaten_kecamatan"],
-        #     "source": item["source"],
-        #     "transmisi": item["transmisi"],
-        #     "tipe_penjual": item["tipe_penjual"],
-        #     'tanggal_diperbaharui_sumber': item["tanggal_diperbaharui_sumber"]
-        # }
-        # headers = {
-        #     'Content-Type': "application/json",
-        #     'cache-control': "no-cache"
-        # }
-        # payload = json.dumps(payload)
-        # response = requests.request("POST", post_url, data=payload, headers=headers)
-        yield item
 
 
 
-
-    # def get_specification(self, response):
-    #     pass
-    #
-    # def parse_result(self, response):
-    #     pass
