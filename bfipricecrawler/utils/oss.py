e1825f0d934d5d808bcf98b137e9124a49eab231
davidbfi <david.hutape@bfi.co.id>
2021-10-14 22:01:22 +0700

    Update : create olx bucket

--- bfipricecrawler/utils/oss.py
+++ bfipricecrawler/utils/oss.py
@@ -7,6 +7,7 @@ auth = oss2.Auth(oss_config['accessKeyId'], oss_config['accessKeySecret'])
 bucket = oss2.Bucket(auth, oss_config['endpoint'], oss_config['bucket_name'])
 file_carmudi = 'FileCrawled/Carmudi/carmudi_{}'.format(int(datetime.now().strftime('%Y%m%d')))
 file_mobil = 'FileCrawled/Mobil123/mobil123_{}'.format(int(datetime.now().strftime('%Y%m%d')))
+file_olx = 'FileCrawled/olx/olx_{}'.format(int(datetime.now().strftime('%Y%m%d')))
 
 
 def create_object(bucket, file):
@@ -17,3 +18,4 @@ def create_object(bucket, file):
 
 create_object(bucket=bucket, file=file_carmudi)
 create_object(bucket=bucket, file=file_mobil)
+create_object(bucket=bucket, file=file_olx)
