aafe4e7d51684b2a2e4c2980edb7fbb54713b6ed
davidbfi <david.hutape@bfi.co.id>
2021-10-18 22:18:52 +0700

    Update : olx spider

--- bfipricecrawler/spiders/mcolx.py
+++ bfipricecrawler/spiders/mcolx.py
@@ -3,12 +3,12 @@ import pandas as pd
 from scrapy.spiders import CrawlSpider, Rule
 from scrapy.linkextractors import LinkExtractor
 
-from bfipricecrawler.items import  MotorItem
-from bfipricecrawler.utils.utils import list_to_dict, price_parser, location_parser, olx_description_parser
+from bfipricecrawler.items import MotorItem
+from bfipricecrawler.utils.utils import list_to_dict, price_parser,  olx_description_parser, olx_location
 
 
 def get_url():
-    data = pd.read_csv('')
+    data = pd.read_csv(r'/home/david/Downloads/csv/olx_motor.csv')
     urls = data['urls']
     urls_ = []
     for url in urls:
@@ -33,18 +33,18 @@ class OlxCrawler(CrawlSpider):
             try:
                 descriptions = olx_description_parser(
                   response.xpath('/html/body/div/div/main/div/div/div/div[4]/section[1]/div/div/div[2]//text()').getall())
-                location = location_parser(response.css('div[class="_2A3Wa"]  ::text').extract_first().split(','))
+                lokasi = olx_location(response.css('div[class="ZGU9S"] ::text').extract())
                 summary_specifications = list_to_dict(response.css('div[class="_3JPEe"] ::text').extract())
                 item["url"] = response.url
                 item['nama'] = response.css('h1[class="_3rJ6e"] ::text').extract_first()
                 item['merek'] = summary_specifications.get('Merek') or ""
                 item['model'] = summary_specifications.get('Model') or ""
                 item["tahun"] = summary_specifications.get('Tahun') or ""
-                item['provinsi'] = location.get('provinsi')
-                item['kabupaten_kecamatan'] = location.get('kabupaten_kota')
+                item['provinsi'] = lokasi.get('provinsi')
+                item['kabupaten_kecamatan'] = lokasi.get('kabupaten_kota')
                 item['harga_offline'] = int(price_parser(response.css('span[class="_2xKfz"] ::text').extract_first()))
                 item['deskripsi'] = descriptions
-                item['lokasi'] = location
+                item['lokasi'] = lokasi
                 item['spesifikasi_ringkas'] = summary_specifications
                 item['sumber'] = 'Olx'
                 yield item
